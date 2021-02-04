d7842382898e988b149d344a407e78cd4fd824dc
Nicolás Sanabria <nicolas.sanabria@imagineapps.co>
2021-02-04 07:54:31 -0500

    removed calls from users api

--- routes/api/user.js
+++ routes/api/user.js
@@ -18,20 +18,6 @@ module.exports = function (router) {
 
   /** Trae los estudiantes de un advisor */
   router.get(
-    "/user/advisor/:advisor",
-    passport.authenticate("jwt", { session: false }),
-    UserController.getByAdvisor
-  );
-
-  /** Trae los estudiantes por tipo */
-  router.get(
-    "/user/tipo/:tipo",
-    passport.authenticate("jwt", { session: false }),
-    UserController.getAllByTipo
-  );
-
-  /** Trae los estudiantes de un advisor */
-  router.get(
     "/user/:id",
     passport.authenticate("jwt", { session: false }),
     UserController.getById
@@ -44,36 +30,10 @@ module.exports = function (router) {
     UserController.update
   );
 
-  /** Agrega servicio médico a un usuario */
-  router.post(
-    "/user/servicio-medico",
-    passport.authenticate("jwt", { session: false }),
-    UserController.addMedicalService
-  );
-
   /** Elimina un usuario por id del usuario */
   router.delete(
     "/user/:id",
     passport.authenticate("jwt", { session: false }),
     UserController.delete
   );
-
-  /** Añade un favorito a la lista de favoritos */
-  router.post(
-    "/user/post-favourite/",
-    passport.authenticate("jwt", { session: false }),
-    UserController.addFavourite
-  );
-  /** Elimina un favorito a la lista de favoritos */
-  router.post(
-    "/user/remove-favourite/",
-    passport.authenticate("jwt", { session: false }),
-    UserController.deleteFavourite
-  );
-  /** Obtiene los favoritos de un usuario */
-  router.get(
-    "/user/favoritos/:id",
-    passport.authenticate("jwt", { session: false }),
-    UserController.getFavourites
-  )
 };
