e6aca9bcae54c5d0ad56c408a0a06f1ad78a2928
Nicolás Sanabria <nicolas.sanabria@imagineapps.co>
2021-02-07 22:27:32 -0500

    added sector model

---
+++ routes/api/sector.js
@@ -0,0 +1,34 @@
+const { Router } = require("express");
+const passport = require("passport");
+const SectorController = require("../../controllers/sector_controller");
+
+module.exports = function (router) {
+    /** Trae todos los usuarios por tipo */
+    router.get(
+      "/sector",
+      passport.authenticate("jwt", { session: false }),
+      SectorController.getAll
+    );
+  
+    /** Gets sector by id */
+    router.get(
+      "/sector/:id",
+      passport.authenticate("jwt", { session: false }),
+      SectorController.get
+    );
+  
+    /** Actualiza una sector por id */
+    router.put(
+      "/sector/:id",
+      passport.authenticate("jwt", { session: false }),
+      SectorController.update
+    );
+  
+    /** Elimina una sector */
+    router.delete(
+      "/sector/:id",
+      passport.authenticate("jwt", { session: false }),
+      SectorController.delete
+    );
+  };
+  
