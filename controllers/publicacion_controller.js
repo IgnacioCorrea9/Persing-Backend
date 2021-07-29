970e6fb75eb84994f2eba356cacedbf13cd5a2ef
LC <lc.pardo@imagineapps.co>
2021-07-28 19:45:46 -0500

    Admin deploy

--- controllers/publicacion_controller.js
+++ controllers/publicacion_controller.js
@@ -6,63 +6,63 @@ const User = require("../models/user");
 
 /** get function to get Publicacion by id. */
 exports.get = function (req, res) {
-  Publicacion.get({ _id: req.params.id }, function (err, result) {
-    if (!err) {
-      return res.json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.get({ _id: req.params.id }, function (err, result) {
+		if (!err) {
+			return res.json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 /** Gets publicaciones and evaluates for each one if user liked it or saved it and modifies model response.
  * FLiters by name and description if query available
  */
 exports.getAllForUser = function (req, res) {
-  Publicacion.getAll({}, function (err, result) {
-    if (!err) {
-      const userId = req.params.user;
-      User.get({ _id: userId }, function (err2, usuario) {
-        if (!err) {
-          if (usuario) {
-            console.log(usuario);
-            result.forEach((element) => {
-              var liked = element.likes.includes(userId);
-              element._doc["liked"] = liked;
-              element._doc["likes"] = element.likes.length;
-              var saved = element.guardados.includes(userId);
-              element._doc["saved"] = saved;
-            });
-            const search = req.query.search;
-            if (search) {
-              result = result.filter(
-                (p) =>
-                  p.titulo.toLowerCase().includes(search) ||
-                  p.texto.toLowerCase().includes(search)
-              );
-            }
-            if (req.query.intereses) {
-              const intereses = req.query.intereses
-                .replace(/\s/g, "")
-                .substring(1)
-                .slice(0, -1)
-                .split(",");
-              usuario.intereses = intereses;
-            }
-            result = result.filter((p) => {
-              return (
-                p.sector && usuario.intereses.includes(p.sector._id.toString())
-              );
-            });
-            console.log(result.length);
-            return res.status(200).json({ success: true, data: result });
-          }
-        }
-      });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.getAll({}, function (err, result) {
+		if (!err) {
+			const userId = req.params.user;
+			User.get({ _id: userId }, function (err2, usuario) {
+				if (!err) {
+					if (usuario) {
+						console.log(usuario);
+						result.forEach((element) => {
+							var liked = element.likes.includes(userId);
+							element._doc["liked"] = liked;
+							element._doc["likes"] = element.likes.length;
+							var saved = element.guardados.includes(userId);
+							element._doc["saved"] = saved;
+						});
+						const search = req.query.search;
+						if (search) {
+							result = result.filter(
+								(p) =>
+									p.titulo.toLowerCase().includes(search) ||
+									p.texto.toLowerCase().includes(search)
+							);
+						}
+						if (req.query.intereses) {
+							const intereses = req.query.intereses
+								.replace(/\s/g, "")
+								.substring(1)
+								.slice(0, -1)
+								.split(",");
+							usuario.intereses = intereses;
+						}
+						result = result.filter((p) => {
+							return (
+								p.sector && usuario.intereses.includes(p.sector._id.toString())
+							);
+						});
+						console.log(result.length);
+						return res.status(200).json({ success: true, data: result });
+					}
+				}
+			});
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 /**
@@ -71,201 +71,216 @@ exports.getAllForUser = function (req, res) {
  * @param {*} res
  */
 exports.getAllDestacadas = function (req, res) {
-  Publicacion.getAll({ destacada: true }, function (err, result) {
-    if (!err) {
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.getAll({ destacada: true }, function (err, result) {
+		if (!err) {
+			return res.status(200).json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
+};
+
+/**
+ * Gets all publicaciones nuevas
+ * @param {*} req
+ * @param {*} res
+ */
+exports.getAllNuevas = function (req, res) {
+	Publicacion.getAll({ nueva: true }, function (err, result) {
+		if (!err) {
+			return res.status(200).json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 /** Gets publicaciones and evaluates for each one if user liked it or saved it and modifies model response.
  * FLiters by name and description if query available
  */
 exports.getAllDestacadasForUser = function (req, res) {
-  Publicacion.getAll({ destacada: true }, function (err, result) {
-    if (!err) {
-      const userId = req.params.user;
-      result.forEach((element) => {
-        var liked = element.likes.includes(userId);
-        element._doc["liked"] = liked;
-        element._doc["likes"] = element.likes.length;
-        var saved = element.guardados.includes(userId);
-        element._doc["saved"] = saved;
-        const search = req.query.search;
-        if (search) {
-          result = result.filter(
-            (p) =>
-              p.titulo.toLowerCase().includes(search) ||
-              p.texto.toLowerCase().includes(search)
-          );
-        }
-      });
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.getAll({ destacada: true }, function (err, result) {
+		if (!err) {
+			const userId = req.params.user;
+			result.forEach((element) => {
+				var liked = element.likes.includes(userId);
+				element._doc["liked"] = liked;
+				element._doc["likes"] = element.likes.length;
+				var saved = element.guardados.includes(userId);
+				element._doc["saved"] = saved;
+				const search = req.query.search;
+				if (search) {
+					result = result.filter(
+						(p) =>
+							p.titulo.toLowerCase().includes(search) ||
+							p.texto.toLowerCase().includes(search)
+					);
+				}
+			});
+			return res.status(200).json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 exports.toggleSave = function (req, res) {
-  Publicacion.get({ _id: req.params.id }, function (err, result) {
-    if (!err) {
-      const userId = req.body.user;
-      const type = req.body.type;
-      const arraySaves = result._doc.guardados;
-      if (type === "add") {
-        if (!arraySaves.includes(userId)) {
-          arraySaves.push(userId);
-        }
-      } else {
-        const index = arraySaves.indexOf(userId);
-        if (index > -1) {
-          arraySaves.splice(index, 1);
-        }
-      }
-      var toSave = {
-        guardados: arraySaves,
-      };
-      Publicacion.updateById(req.params.id, toSave, function (err, result) {
-        if (!err) {
-          return res.status(200).json({ success: true, data: result });
-        } else {
-          return res.status(500).send({ success: false, error: err }); // 500 error
-        }
-      });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.get({ _id: req.params.id }, function (err, result) {
+		if (!err) {
+			const userId = req.body.user;
+			const type = req.body.type;
+			const arraySaves = result._doc.guardados;
+			if (type === "add") {
+				if (!arraySaves.includes(userId)) {
+					arraySaves.push(userId);
+				}
+			} else {
+				const index = arraySaves.indexOf(userId);
+				if (index > -1) {
+					arraySaves.splice(index, 1);
+				}
+			}
+			var toSave = {
+				guardados: arraySaves,
+			};
+			Publicacion.updateById(req.params.id, toSave, function (err, result) {
+				if (!err) {
+					return res.status(200).json({ success: true, data: result });
+				} else {
+					return res.status(500).send({ success: false, error: err }); // 500 error
+				}
+			});
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 exports.getAllSavedByUser = function (req, res) {
-  Publicacion.getAll(
-    { guardados: { $in: [req.params.user] } },
-    function (err, result) {
-      if (!err) {
-        const userId = req.params.user;
-        result.forEach((element) => {
-          var liked = element.likes.includes(userId);
-          element._doc["liked"] = liked;
-          element._doc["likes"] = element.likes.length;
-          var saved = element.guardados.includes(userId);
-          element._doc["saved"] = saved;
-        });
-        return res.status(200).json({ success: true, data: result });
-      } else {
-        return res.status(500).send({ success: false, error: err }); // 500 error
-      }
-    }
-  );
+	Publicacion.getAll(
+		{ guardados: { $in: [req.params.user] } },
+		function (err, result) {
+			if (!err) {
+				const userId = req.params.user;
+				result.forEach((element) => {
+					var liked = element.likes.includes(userId);
+					element._doc["liked"] = liked;
+					element._doc["likes"] = element.likes.length;
+					var saved = element.guardados.includes(userId);
+					element._doc["saved"] = saved;
+				});
+				return res.status(200).json({ success: true, data: result });
+			} else {
+				return res.status(500).send({ success: false, error: err }); // 500 error
+			}
+		}
+	);
 };
 
 exports.toggleLike = function (req, res) {
-  Publicacion.get({ _id: req.params.id }, function (err, result) {
-    if (!err) {
-      const userId = req.body.user;
-      const type = req.body.type;
-      const arrayLikes = result._doc.likes;
-      if (type === "add") {
-        if (!arrayLikes.includes(userId)) {
-          arrayLikes.push(userId);
-        }
-      } else {
-        const index = arrayLikes.indexOf(userId);
-        if (index > -1) {
-          arrayLikes.splice(index, 1);
-        }
-      }
-      var toSave = {
-        likes: arrayLikes,
-      };
-      Publicacion.updateById(req.params.id, toSave, function (err, result) {
-        if (!err) {
-          const like = {
-            publicacion: req.params.id,
-            usuario: userId,
-          };
-          Like.create(like, function (err2, result2) {
-            if (!err2) {
-              console.log(result2);
-              console.log("created like");
-            }
-          });
-          return res.status(200).json({ success: true, data: result });
-        } else {
-          return res.status(500).send({ success: false, error: err }); // 500 error
-        }
-      });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.get({ _id: req.params.id }, function (err, result) {
+		if (!err) {
+			const userId = req.body.user;
+			const type = req.body.type;
+			const arrayLikes = result._doc.likes;
+			if (type === "add") {
+				if (!arrayLikes.includes(userId)) {
+					arrayLikes.push(userId);
+				}
+			} else {
+				const index = arrayLikes.indexOf(userId);
+				if (index > -1) {
+					arrayLikes.splice(index, 1);
+				}
+			}
+			var toSave = {
+				likes: arrayLikes,
+			};
+			Publicacion.updateById(req.params.id, toSave, function (err, result) {
+				if (!err) {
+					const like = {
+						publicacion: req.params.id,
+						usuario: userId,
+					};
+					Like.create(like, function (err2, result2) {
+						if (!err2) {
+							console.log(result2);
+							console.log("created like");
+						}
+					});
+					return res.status(200).json({ success: true, data: result });
+				} else {
+					return res.status(500).send({ success: false, error: err }); // 500 error
+				}
+			});
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 /** get function to get all Publicacion. */
 exports.getAll = function (req, res) {
-  Publicacion.getAll({}, function (err, result) {
-    if (!err) {
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.getAll({}, function (err, result) {
+		if (!err) {
+			return res.status(200).json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 /** get function to get all Publicacion. */
 exports.getAllByEmpresa = function (req, res) {
-  Publicacion.getAll({ empresa: req.params.empresa }, function (err, result) {
-    if (!err) {
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.getAll({ empresa: req.params.empresa }, function (err, result) {
+		if (!err) {
+			return res.status(200).json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 /** get function to get all Publicacion. */
 exports.getAllBySector = function (req, res) {
-  Publicacion.getAll({ sector: req.params.sector }, function (err, result) {
-    if (!err) {
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.getAll({ sector: req.params.sector }, function (err, result) {
+		if (!err) {
+			return res.status(200).json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 /** update function to update Publicacion by id. */
 exports.update = function (req, res) {
-  Publicacion.updateById(req.params.id, req.body, function (err, result) {
-    if (!err) {
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.updateById(req.params.id, req.body, function (err, result) {
+		if (!err) {
+			return res.status(200).json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 /** update function to update Publicacion by id. */
 exports.create = function (req, res) {
-  Publicacion.create(req.body, function (err, result) {
-    if (!err) {
-      return res.status(201).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.create(req.body, function (err, result) {
+		if (!err) {
+			return res.status(201).json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
 
 /** remove function to remove Publicacion by id. */
 exports.delete = function (req, res) {
-  Publicacion.removeById({ _id: req.params.id }, function (err, result) {
-    if (!err) {
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
+	Publicacion.removeById({ _id: req.params.id }, function (err, result) {
+		if (!err) {
+			return res.status(200).json({ success: true, data: result });
+		} else {
+			return res.status(500).send({ success: false, error: err }); // 500 error
+		}
+	});
 };
--- models/publicacion.js
+++ models/publicacion.js
@@ -1,74 +1,75 @@
 const mongoose = require("mongoose");
 
 const PublicacionSchema = mongoose.Schema({
-  empresa: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Empresa",
-    required: false,
-  },
-  sector: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Sector",
-    required: false,
-  },
-  titulo: { type: String, required: false },
-  link: { type: String, required: false },
-  texto: { type: String, required: false },
-  foto: { type: String, required: false },
-  video: { type: String, required: false },
-  likes: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
-  ],
-  guardados: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
-  ],
-  comentarios: { type: Number, required: false, default: 0 },
-  alcanzados: { type: Number, required: false, default: 0 },
-  inversion: { type: Number, required: false },
-  destacada: { type: Boolean, required: true, default: false },
-  vtr: { type: String, required: false },
-  engagement: { type: String, required: false },
-  cpc: { type: String, required: false },
-  cpm: { type: String, required: false },
-  createdAt: { type: Date, required: false, default: Date.now },
+	empresa: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Empresa",
+		required: false,
+	},
+	sector: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Sector",
+		required: false,
+	},
+	titulo: { type: String, required: false },
+	link: { type: String, required: false },
+	texto: { type: String, required: false },
+	foto: { type: String, required: false },
+	video: { type: String, required: false },
+	likes: [
+		{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+	],
+	guardados: [
+		{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
+	],
+	comentarios: { type: Number, required: false, default: 0 },
+	alcanzados: { type: Number, required: false, default: 0 },
+	inversion: { type: Number, required: false },
+	destacada: { type: Boolean, required: false, default: false },
+	nueva: { type: Boolean, required: false, default: false },
+	vtr: { type: String, required: false },
+	engagement: { type: String, required: false },
+	cpc: { type: String, required: false },
+	cpm: { type: String, required: false },
+	createdAt: { type: Date, required: false, default: Date.now },
 });
 
 PublicacionSchema.statics = {
-  get: function (query, callback) {
-    this.findOne(query, { password: 0 })
-      .sort("-createdAt")
-      .populate("empresa sector")
-      .exec(callback);
-  },
-  getAll: function (query, callback) {
-    this.find(query, { password: 0 })
-      .sort("-createdAt")
-      .populate("empresa sector")
-      .exec(callback);
-  },
-  updateById: function (id, updateData, callback) {
-    this.findOneAndUpdate(
-      { _id: id },
-      { $set: updateData },
-      { new: true },
-      callback
-    );
-  },
+	get: function (query, callback) {
+		this.findOne(query, { password: 0 })
+			.sort("-createdAt")
+			.populate("empresa sector")
+			.exec(callback);
+	},
+	getAll: function (query, callback) {
+		this.find(query, { password: 0 })
+			.sort("-createdAt")
+			.populate("empresa sector")
+			.exec(callback);
+	},
+	updateById: function (id, updateData, callback) {
+		this.findOneAndUpdate(
+			{ _id: id },
+			{ $set: updateData },
+			{ new: true },
+			callback
+		);
+	},
 
-  removeById: function (removeData, callback) {
-    this.findOneAndRemove(removeData, callback);
-  },
-  create: function (data, callback) {
-    const user = new this(data);
-    user.save(callback);
-  },
+	removeById: function (removeData, callback) {
+		this.findOneAndRemove(removeData, callback);
+	},
+	create: function (data, callback) {
+		const user = new this(data);
+		user.save(callback);
+	},
 
-  getNoPopulate: function (query, callback) {
-    this.findOne(query, { password: 0 }).exec(callback);
-  },
+	getNoPopulate: function (query, callback) {
+		this.findOne(query, { password: 0 }).exec(callback);
+	},
 };
 
 const Publicacion = (module.exports = mongoose.model(
-  "Publicacion",
-  PublicacionSchema
+	"Publicacion",
+	PublicacionSchema
 ));
