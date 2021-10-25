3b4f3f397603535a6451a435ee29ab8232cac271
Alejandra Pérez Mondragón <Alejandra.perez@imagineapps.co>
2021-10-25 10:06:16 -0500

    Add passport authentication

--- routes/api/destacado.js
+++ routes/api/destacado.js
@@ -6,52 +6,52 @@ module.exports = function (router) {
 	/** Trae todos los usuarios por tipo */
 	router.get(
 		"/destacado/user/:user",
-		/* passport.authenticate("jwt", { session: false }), */
+		passport.authenticate("jwt", { session: false }), 
 		PublicacionController.getAllForUser
 	);
 
 	router.get(
 		"/destacado",
-		/* passport.authenticate("jwt", { session: false }), */
+		passport.authenticate("jwt", { session: false }),
 		PublicacionController.getAll
 	);
 
 	router.get(
 		"/destacado/seccion/:seccion/usuario/:user",
-		/* passport.authenticate("jwt", { session: false }), */
+		passport.authenticate("jwt", { session: false }), 
 		PublicacionController.getAllBySeccion
 	);
 
 	router.put(
 		"/destacado/toggle-like/:id",
-		/* passport.authenticate("jwt", { session: false }), */
+		passport.authenticate("jwt", { session: false }), 
 		PublicacionController.toggleLike
 	);
 
 	router.post(
 		"/destacado",
-		/* passport.authenticate("jwt", { session: false }), */
+		passport.authenticate("jwt", { session: false }), 
 		PublicacionController.create
 	);
 
 	/** Gets destacado by id */
 	router.get(
 		"/destacado/:id",
-		/* passport.authenticate("jwt", { session: false }), */
+		passport.authenticate("jwt", { session: false }), 
 		PublicacionController.get
 	);
 
 	/** Actualiza una destacado por id */
 	router.put(
 		"/destacado/:id",
-		/* passport.authenticate("jwt", { session: false }), */
+		passport.authenticate("jwt", { session: false }), 
 		PublicacionController.update
 	);
 
 	/** Elimina una destacado */
 	router.delete(
 		"/destacado/:id",
-		/* passport.authenticate("jwt", { session: false }), */
+		passport.authenticate("jwt", { session: false }), 
 		PublicacionController.delete
 	);
 };
--- routes/api/like.js
+++ routes/api/like.js
@@ -6,41 +6,41 @@ module.exports = function (router) {
   /** Trae todos los usuarios por tipo */
   router.get(
     "/like",
-    /* passport.authenticate("jwt", { session: false }), */
+    passport.authenticate("jwt", { session: false }),
     LikeController.getAll
   );
 
   router.get(
     "/like/publicacion/:publicacion",
-    /* passport.authenticate("jwt", { session: false }), */
+    passport.authenticate("jwt", { session: false }),
     LikeController.countByPublicacion
   );
 
   /** Trae todos los usuarios por tipo */
   router.post(
     "/like",
-    /* passport.authenticate("jwt", { session: false }), */
+    passport.authenticate("jwt", { session: false }),
     LikeController.create
   );
 
   /** Gets like by id */
   router.get(
     "/like/:id",
-    /* passport.authenticate("jwt", { session: false }), */
+    passport.authenticate("jwt", { session: false }),
     LikeController.get
   );
 
   /** Actualiza una like por id */
   router.put(
     "/like/:id",
-    /* passport.authenticate("jwt", { session: false }), */
+    passport.authenticate("jwt", { session: false }),
     LikeController.update
   );
 
   /** Elimina una like */
   router.delete(
     "/like/:id",
-    /* passport.authenticate("jwt", { session: false }), */
+    passport.authenticate("jwt", { session: false }),
     LikeController.delete
   );
 };
