41cef20c705e3e67da1b901db936e3d45f5a7450
Luis Zapata <luis.zapata@imagineapps.co>
2021-11-18 10:56:43 -0500

    Updated getAllbySector module and posts endpoints

--- controllers/publicacion_controller.js
+++ controllers/publicacion_controller.js
@@ -338,6 +338,15 @@ exports.getAllBySector = function (req, res) {
     { sector: req.params.sector, deletedAt: { $exists: false } },
     function (err, result) {
       if (!err) {
+          const userId = req.params.user;
+          result.forEach((element) => {
+            var liked = element.likes.includes(userId);
+            element._doc["liked"] = liked;
+            element._doc["likes"] = element.likes.length;
+            var saved = element.guardados.includes(userId);
+            element._doc["saved"] = saved;
+          });
+          return res.status(200).json({ success: true, data: result });
         return res.status(200).json({ success: true, data: result });
       } else {
         return res.status(500).send({ success: false, error: err }); // 500 error
