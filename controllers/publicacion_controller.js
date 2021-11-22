5dbc374c06bc564b531708fb9e09fd47cfc19057
Alejandra Pérez Mondragón <Alejandra.perez@imagineapps.co>
2021-11-22 09:47:22 -0500

    Lint controller

--- controllers/publicacion_controller.js
+++ controllers/publicacion_controller.js
@@ -20,8 +20,92 @@ exports.get = function (req, res) {
  * FLiters by name and description if query available
  */
 exports.getAllForUser = function (req, res) {
+  Publicacion.getAll({ deletedAt: { $exists: false } }, function (err, result) {
+    if (!err) {
+      const userId = req.params.user;
+      User.get({ _id: userId }, function (err2, usuario) {
+        if (!err) {
+          if (usuario) {
+            result.forEach((element) => {
+              var liked = element.likes.includes(userId);
+              element._doc["liked"] = liked;
+              element._doc["likes"] = element.likes.length;
+              var saved = element.guardados.includes(userId);
+              element._doc["saved"] = saved;
+            });
+            const search = req.query.search;
+            if (search) {
+              result = result.filter(
+                (p) =>
+                  p.titulo.toLowerCase().includes(search) ||
+                  p.texto.toLowerCase().includes(search)
+              );
+            }
+            if (req.query.intereses) {
+              const intereses = req.query.intereses
+                .replace(/\s/g, "")
+                .substring(1)
+                .slice(0, -1)
+                .split(",");
+              usuario.intereses = intereses;
+            }
+            result = result.filter((p) => {
+              return (
+                p.sector && usuario.intereses.includes(p.sector._id.toString())
+              );
+            });
+            result = _.shuffle(result);
+            return res.status(200).json({ success: true, data: result });
+          }
+        }
+      });
+    } else {
+      return res.status(500).send({ success: false, error: err }); // 500 error
+    }
+  });
+};
+
+/**
+ * Gets all publicaciones destacadas
+ * @param {*} req
+ * @param {*} res
+ */
+exports.getAllDestacadas = function (req, res) {
   Publicacion.getAll(
-    { deletedAt: { $exists: false } },
+    { destacada: true, deletedAt: { $exists: false } },
+    function (err, result) {
+      if (!err) {
+        return res.status(200).json({ success: true, data: result });
+      } else {
+        return res.status(500).send({ success: false, error: err }); // 500 error
+      }
+    }
+  );
+};
+
+/**
+ * Gets all publicaciones nuevas
+ * @param {*} req
+ * @param {*} res
+ */
+exports.getAllNuevas = function (req, res) {
+  Publicacion.getAll({ nueva: true }, function (err, result) {
+    if (!err) {
+      return res.status(200).json({ success: true, data: result });
+    } else {
+      return res.status(500).send({ success: false, error: err }); // 500 error
+    }
+  });
+};
+
+/**
+ * Gets all publicaciones nuevas
+ * @param {*} req
+ * @param {*} res
+ */
+exports.getNuevasByUser = function (req, res) {
+  Publicacion.getAll(
+    { nueva: true, deletedAt: { $exists: false } },
     function (err, result) {
       if (!err) {
         const userId = req.params.user;
@@ -69,118 +153,37 @@ exports.getAllForUser = function (req, res) {
   );
 };
 
-/**
- * Gets all publicaciones destacadas
- * @param {*} req
- * @param {*} res
+/** Gets publicaciones and evaluates for each one if user liked it or saved it and modifies model response.
+ * FLiters by name and description if query available
  */
-exports.getAllDestacadas = function (req, res) {
+exports.getAllDestacadasForUser = function (req, res) {
   Publicacion.getAll(
     { destacada: true, deletedAt: { $exists: false } },
     function (err, result) {
       if (!err) {
+        const userId = req.params.user;
+        result.forEach((element) => {
+          var liked = element.likes.includes(userId);
+          element._doc["liked"] = liked;
+          element._doc["likes"] = element.likes.length;
+          var saved = element.guardados.includes(userId);
+          element._doc["saved"] = saved;
+          const search = req.query.search;
+          if (search) {
+            result = result.filter(
+              (p) =>
+                p.titulo.toLowerCase().includes(search) ||
+                p.texto.toLowerCase().includes(search)
+            );
+          }
+        });
+        result = _.shuffle(result);
         return res.status(200).json({ success: true, data: result });
       } else {
         return res.status(500).send({ success: false, error: err }); // 500 error
       }
     }
   );
-};
-
-/**
- * Gets all publicaciones nuevas
- * @param {*} req
- * @param {*} res
- */
-exports.getAllNuevas = function (req, res) {
-  Publicacion.getAll({ nueva: true }, function (err, result) {
-    if (!err) {
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
-};
-
-/**
- * Gets all publicaciones nuevas
- * @param {*} req
- * @param {*} res
- */
-exports.getNuevasByUser = function (req, res) {
-  Publicacion.getAll({ nueva: true, deletedAt: { $exists: false} }, function (err, result) {
-    if (!err) {
-      const userId = req.params.user;
-      User.get({ _id: userId }, function (err2, usuario) {
-        if (!err) {
-          if (usuario) {
-            result.forEach((element) => {
-              var liked = element.likes.includes(userId);
-              element._doc["liked"] = liked;
-              element._doc["likes"] = element.likes.length;
-              var saved = element.guardados.includes(userId);
-              element._doc["saved"] = saved;
-            });
-            const search = req.query.search;
-            if (search) {
-              result = result.filter(
-                (p) =>
-                  p.titulo.toLowerCase().includes(search) ||
-                  p.texto.toLowerCase().includes(search)
-              );
-            }
-            if (req.query.intereses) {
-              const intereses = req.query.intereses
-                .replace(/\s/g, "")
-                .substring(1)
-                .slice(0, -1)
-                .split(",");
-              usuario.intereses = intereses;
-            }
-            result = result.filter((p) => {
-              return (
-                p.sector && usuario.intereses.includes(p.sector._id.toString())
-              );
-            });
-            result = _.shuffle(result);
-            return res.status(200).json({ success: true, data: result });
-          }
-        }
-      });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
-};
-
-/** Gets publicaciones and evaluates for each one if user liked it or saved it and modifies model response.
- * FLiters by name and description if query available
- */
-exports.getAllDestacadasForUser = function (req, res) {
-  Publicacion.getAll({ destacada: true, deletedAt: {$exists: false} }, function (err, result) {
-    if (!err) {
-      const userId = req.params.user;
-      result.forEach((element) => {
-        var liked = element.likes.includes(userId);
-        element._doc["liked"] = liked;
-        element._doc["likes"] = element.likes.length;
-        var saved = element.guardados.includes(userId);
-        element._doc["saved"] = saved;
-        const search = req.query.search;
-        if (search) {
-          result = result.filter(
-            (p) =>
-              p.titulo.toLowerCase().includes(search) ||
-              p.texto.toLowerCase().includes(search)
-          );
-        }
-      });
-      result = _.shuffle(result);
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res.status(500).send({ success: false, error: err }); // 500 error
-    }
-  });
 };
 
 exports.toggleSave = function (req, res) {
@@ -217,7 +220,7 @@ exports.toggleSave = function (req, res) {
 
 exports.getAllSavedByUser = function (req, res) {
   Publicacion.getAll(
-    { guardados: { $in: [req.params.user] }, deletedAt: {$exists: false} },
+    { guardados: { $in: [req.params.user] }, deletedAt: { $exists: false } },
     function (err, result) {
       if (!err) {
         const userId = req.params.user;
@@ -263,7 +266,6 @@ exports.toggleLike = function (req, res) {
           };
           Like.create(like, function (err2, result2) {
             if (!err2) {
-              console.log(result2);
               console.log("created like");
             }
           });
@@ -280,16 +282,13 @@ exports.toggleLike = function (req, res) {
 
 /** get function to get all Publicacion. */
 exports.getAll = function (req, res) {
-  Publicacion.getAll(
-    { deletedAt: { $exists: false } },
-    function (err, result) {
-      if (!err) {
-        return res.status(200).json({ success: true, data: result });
-      } else {
-        return res.status(500).send({ success: false, error: err }); // 500 error
-      }
+  Publicacion.getAll({ deletedAt: { $exists: false } }, function (err, result) {
+    if (!err) {
+      return res.status(200).json({ success: true, data: result });
+    } else {
+      return res.status(500).send({ success: false, error: err }); // 500 error
     }
-  );
+  });
 };
 
 /** get function to get all Publicacion by empresa and user */
@@ -339,15 +338,15 @@ exports.getAllBySector = function (req, res) {
     { sector: req.params.sector, deletedAt: { $exists: false } },
     function (err, result) {
       if (!err) {
-          const userId = req.params.user;
-          result.forEach((element) => {
-            var liked = element.likes.includes(userId);
-            element._doc["liked"] = liked;
-            element._doc["likes"] = element.likes.length;
-            var saved = element.guardados.includes(userId);
-            element._doc["saved"] = saved;
-          });
-          return res.status(200).json({ success: true, data: result });
+        const userId = req.params.user;
+        result.forEach((element) => {
+          var liked = element.likes.includes(userId);
+          element._doc["liked"] = liked;
+          element._doc["likes"] = element.likes.length;
+          var saved = element.guardados.includes(userId);
+          element._doc["saved"] = saved;
+        });
+        return res.status(200).json({ success: true, data: result });
         return res.status(200).json({ success: true, data: result });
       } else {
         return res.status(500).send({ success: false, error: err }); // 500 error
