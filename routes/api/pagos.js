95a58fef47e6c69fedfaf44ed72c09aa59ad74cb
Luis Zapata <luis.zapata@imagineapps.co>
2022-09-03 09:45:19 -0500

    Se corrige algoritmo para otorgar recompensas al pagar publicaciones

--- routes/api/pagos.js
+++ routes/api/pagos.js
@@ -1,44 +1,44 @@
-const { Router } = require('express');
-const passport = require('passport');
-const PagosController = require('../../controllers/pagos_controller');
+const { Router } = require("express");
+const passport = require("passport");
+const PagosController = require("../../controllers/pagos_controller");
 
 module.exports = function (router) {
   router.get(
-    '/pagos',
-    passport.authenticate('jwt', { session: false }),
+    "/pagos",
+    passport.authenticate("jwt", { session: false }),
     PagosController.getAll
   );
 
   router.post(
-    '/pagos',
-    passport.authenticate('jwt', { session: false }),
+    "/pagos",
+    passport.authenticate("jwt", { session: false }),
     PagosController.create
   );
 
   router.get(
-    '/pagos/empresa/:id',
-    passport.authenticate('jwt', { session: false }),
+    "/pagos/empresa/:id",
+    passport.authenticate("jwt", { session: false }),
     PagosController.getEmpresa
   );
 
   /** Gets pagos by id */
   router.get(
-    '/pagos/:id',
-    passport.authenticate('jwt', { session: false }),
+    "/pagos/:id",
+    passport.authenticate("jwt", { session: false }),
     PagosController.get
   );
 
   /** Actualiza una pagos por id */
   router.put(
-    '/pagos/:id',
-    passport.authenticate('jwt', { session: false }),
+    "/pagos/:id",
+    passport.authenticate("jwt", { session: false }),
     PagosController.update
   );
 
   /** Elimina una pagos */
   router.delete(
-    '/pagos/:id',
-    passport.authenticate('jwt', { session: false }),
+    "/pagos/:id",
+    passport.authenticate("jwt", { session: false }),
     PagosController.delete
   );
 };
